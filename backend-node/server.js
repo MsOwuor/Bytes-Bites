de700232ab03e4c43d2d0d16a81fe3dd7e43c8eb
MsOwuor <immaculateadhimabo79@gmail.com>
2024-07-02 05:59:09 -0700

    backend node

---
+++ backend-node/server.js
@@ -0,0 +1,23 @@
+const authRoutes = require('./routes/auth');
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('API is running...'));
+app.use('/api/auth', authRoutes);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
